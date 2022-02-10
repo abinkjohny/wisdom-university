db4ed9d13fcef55e36a5ed7aa8bc72b7446c1bdd
Abin K Johny <abinkjohny2011@gmail.com>
2022-02-10 16:11:32 +0530

    Added the main index.js file into the wisdome-university-theme directory and made the necessary changes in functions.php to make the changes into effect. Also modified header.php with new url of images

--- app/public/wp-content/themes/wisdom-university-theme/functions.php
+++ app/public/wp-content/themes/wisdom-university-theme/functions.php
@@ -6,6 +6,8 @@ function university_files() {
     wp_enqueue_style('university_additional_style', get_theme_file_uri('index.css')); // function to import css
     wp_enqueue_style('custom_google_fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto:ital,wght@0,100;0,300;0,400;0,700;1,400;1,700&display=swap'); //api
     wp_enqueue_style('university_additional_style', 'https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css'); //api
+
+    wp_enqueue_script('university_main_js', get_theme_file_uri('js/index.js'), NULL, '1.0', true);
 }
 
 

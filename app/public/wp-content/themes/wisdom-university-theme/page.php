3bde54dd83fb24cb928468b460388a090e62991b
Abin K Johny <abinkjohny2011@gmail.com>
2022-05-15 11:44:35 +0530

    Added the logic to display side-bar nav menu in case of pages have parent <--> child relationship

--- app/public/wp-content/themes/wisdom-university-theme/page.php
+++ app/public/wp-content/themes/wisdom-university-theme/page.php
@@ -16,40 +16,83 @@ while(have_posts()) {
         </div>
         
         <div class="container container--narrow page-section">
-        <?php 
-          $theParent = wp_get_post_parent_id(get_the_ID());
-          $theCount = count(get_pages('child_of', $theParent));
-          if($theParent) {
-          ?> 
-              <div class="metabox metabox--position-up metabox--with-home-link">
-                <p>
-                  <a class="metabox__blog-home-link" href="/about-us"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(wp_get_post_parent_id(get_the_ID())); ?></a> <span class="metabox__main"><?php echo get_the_title(); ?></span>
-                </p>
-              </div>
-          <?php
-          }
-        ?>
-
-              <div class="page-links">
-                <h2 class="page-links__title"><a href="/about-us">About Us</a></h2>
-                <ul class="min-list">
-                  <li class="current_page_item"><a href="/our-history">Our History</a></li>
-                  <li><a href="/our-goals">Our Goals</a></li>
-                </ul>
-              </div>
-
-              <div class="generic-content">
-              <p><?php the_content(); ?></p>
-              </div>
-            </div>
-        <?php
+    <?php
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) {
+    ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_the_permalink($theParent); ?>"> <i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title(wp_get_post_parent_id(get_the_ID())); ?></a> <span class="metabox__main"><?php echo get_the_title(); ?></span>
+        </p>
+      </div>
+    <?php
     }
-   
+
+    $theParent = wp_get_post_parent_id(get_the_ID());
+
+    if ($theParent) {
+      $findChildOf = $theParent;
+    } else {
+      $findChildOf = get_the_ID();
+    }
+
+    $args = array(
+      'child_of' => $findChildOf,
+      'sort_order' => 'DESC'
+    );
+
+    $childPages = get_pages($args);
+
+    if ($childPages) {
+
+    ?>
+
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?php echo get_the_permalink($findChildOf); ?>"><?php echo get_the_title($findChildOf); ?></a></h2>
+        <ul class="min-list">
+          <?php
+          foreach ($childPages as $key => $childPage) {
+            try {
+              //7.3 and later
+              if ($key === array_key_first($childPages)) {
+          ?>
+                <li <?php if(get_the_ID() == $childPage->ID)  ?>><a href="<?php echo get_the_permalink($childPage->ID); ?>"><?php echo get_the_title($childPage->ID); ?></a></li>
+              <?php
+              } else {
+              ?>
+                <li <?php if(get_the_ID() == $childPage->ID)  ?>><a href="<?php echo get_the_permalink($childPage->ID); ?>"><?php echo get_the_title($childPage->ID); ?></a></li>
+              <?php
+              }
+            } catch (\Throwable $th) {
+              if (WP_DEBUG === true) {
+                error_log($th->getMessage());
+              }
+              //php 7.2 older
+              reset($childPages);
+              if ($key === key($childPages)) {
+              ?>
+                <li <?php if(get_the_ID() == $childPage->ID)  ?>><a href="<?php echo get_the_permalink($childPage->ID); ?>"><?php echo get_the_title($childPage->ID); ?></a></li>
+              <?php
+              }
+              ?>
+              <li <?php if(get_the_ID() == $childPage->ID)  ?>><a href="<?php echo get_the_permalink($childPage->ID); ?>"><?php echo get_the_title($childPage->ID); ?></a></li>
+          <?php
+            }
+          }
+          ?>
+        </ul>
+      </div>
+
+    <?php
+    }
+    ?>
+
+    <div class="generic-content">
+      <p><?php the_content(); ?></p>
+    </div>
+  </div>
+<?php
+}
+
 
 get_footer();
-
-?>
-
-<pre>
-    <?php var_dump($theCount) ; ?>
-</pre>
